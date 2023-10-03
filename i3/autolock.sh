4c52cc421fb714215ba03a67581ab746a7d2e612
Martin Đaković <martin@tuckdesign.com>
2023-10-03 11:46:45 +0200

    Update autolock.sh

--- i3/autolock.sh
+++ i3/autolock.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-if [ $(grep -r "RUNNING" /proc/asound | wc -l) -eq 0 ]; then
+
+playerctlstatus=$(playerctl status 2> /dev/null)
+playerctlstatusrandom=$(playerctl -p somethingsomething status 2> /dev/null)
+
+if [ "$playerctlstatus" = "$playerctlstatusrandom" ]; then
 	xset dpms force suspend && dunstctl set-paused true && i3lock -i ~/.local/share/wallpapers/i3-lockscreen.png -u -n; dunstctl set-paused false
 fi

b152de56a582d3c69024e42920dc6fae26a85925
Martin Đaković <martin@tuckdesign.com>
2023-10-07 12:09:18 +0200

    Update update.sh

--- polybar/update.sh
+++ polybar/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-aptupgrades=$(apt-get -s upgrade | tail -1 | cut -c-1)
+aptupgrades=$(apt-get -s upgrade | grep upgraded, | cut -c-1)
 
 if [ "$aptupgrades" = "0" ]; then
     echo ""
 else
-    echo "%{F#5d75a1}%{F-}"
+    echo "%{F#5d75a1} $aptupgrades%{F-}"
 fi

cff274c145afd6478c92d509702e8478419db5ff
Martin Đaković <94227158+martin-djakovic@users.noreply.github.com>
2023-12-08 18:31:24 +0100

    Lock changes keyboard layout to US

--- i3/autolock.sh
+++ i3/autolock.sh
@@ -4,5 +4,5 @@ playerctlstatus=$(playerctl status 2> /dev/null)
 playerctlstatusrandom=$(playerctl -p somethingsomething status 2> /dev/null)
 
 if [ "$playerctlstatus" = "$playerctlstatusrandom" ]; then
-	xset dpms force suspend && dunstctl set-paused true && i3lock -i ~/.local/share/wallpapers/i3-lockscreen.png -u -n; dunstctl set-paused false
+    dunstctl set-paused true && setxkbmap us && xset dpms force suspend && i3lock -i ~/.local/share/wallpapers/i3-lockscreen.png -u -n; dunstctl set-paused false && setxkbmap -layout "us,rs,rs" -variant ",latinyz,"
 fi

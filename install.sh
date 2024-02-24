ceffdd917f57c3ac6acbb65f0216657bde2ec3ee
Martin Đaković <94227158+martin-djakovic@users.noreply.github.com>
2024-02-24 10:49:38 +0100

    Added --no-startup-id where needed, fixed security issues

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ apt install software-properties-common -y
 apt-add-repository non-free -y
 apt-add-repository contrib -y
 dpkg --add-architecture i386
+
 echo "deb https://apt.enpass.io/ stable main" > \
   /etc/apt/sources.list.d/enpass.list
 wget -O - https://apt.enpass.io/keys/enpass-linux.key | tee /etc/apt/trusted.gpg.d/enpass.asc
@@ -18,13 +19,16 @@ install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packa
 sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 apt update
+
 wget https://launcher.mojang.com/download/Minecraft.deb
 wget https://github.com/shiftkey/desktop/releases/download/release-3.3.1-linux1/GitHubDesktop-linux-amd64-3.3.1-linux1.deb
+
 apt install xorg i3 nvidia-driver nvidia-settings nvidia-driver-libs:i386 pulseaudio alsa-utils steam gimp inkscape lxappearance thunderbird firefox-esr alacritty thunar vim git unzip shotwell celluloid gvfs-backends samba cifs-utils smbclient rofi i3blocks lightdm nitrogen picom xinput maim xclip xdotool libsdl2-2.0-0 libsdl2-dev libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-ttf-2.0-0 libsdl2-ttf-dev libsdl2-mixer-2.0-0 libsdl2-mixer-dev gtk2-engines-murrine gtk2-engines-pixbuf rhythmbox xautolock libavcodec-extra playerctl gnome-disk-utility gufw enpass libreoffice libreoffice-gtk3 code printer-driver-hpcups gamemode openvpn openssl openresolv transmission-gtk rsync timeshift pavucontrol gdb flatpak galculator gedit kbdd ./Minecraft.deb ./GitHubDesktop-linux-amd64-3.3.1-linux1.deb
 apt install --no-install-recommends lxsession lxpolkit -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install com.discordapp.Discord
 apt autoremove dmenu xterm libreoffice-math libreoffice-draw libreoffice-draw libreoffice-impress libreoffice-base -y
+
 mkdir /home/martin/.local/share/
 cp .Xresources /home/martin/
 cp -r themes/ /usr/share/
@@ -41,16 +45,28 @@ cp -r i3blocks/ /home/martin/.config/
 cp -r dunst/ /home/martin/.config/
 cp -r rofi/ /home/martin/.config/
 mkdir /home/martin/Pictures/screenshots
+
 chown -R martin:martin /home/martin/.local
 chown -R martin:martin /home/martin/.config
 chown -R martin:martin /home/martin/.Xresources
+chown -R martin:martin /home/martin/Pictures/screenshots
+
 timedatectl set-local-rtc 0
+
 xdg-settings set default-web-browser firefox-esr.desktop
 xdg-mime default thunar.desktop inode/directory
+
 mkdir /boot/grub/theme
 cp -r grub-theme-debian/* /boot/grub/theme/
 cp -r .fonts/ /home/martin/
 cp grub /etc/default/
+
+chown root:root /etc/default/grub
+chown -R root:root /boot/grub/theme/
+chown root:root /home/martin/.config/i3/vpn.sh
+chmod -w /etc/default/grub
+chmod -w /home/martin/.config/i3/vpn.sh
+
 update-grub
 
 echo "Don't forget to copy VPN files to /etc/openvpn"
